fa8cbda8bc221c2b7d8b4f54108714a5d2da2cae
Arman <mdalarmansorker@gmail.com>
2024-08-17 01:03:35 +0600

    card added on dashboard with daisyUI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,12 +6,25 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col md:flex-col-2 lg:flex-col-3 gap-3">
+            <div class="card bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title">{{__('Cash In')}}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
                 </div>
             </div>
-        </div>
+            <div class="card bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title">{{__('Cash Out')}}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                </div>
+            </div>
+            <div class="card bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title">{{__('Rest')}}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                </div>
+            </div>
+        </div>   
     </div>
 </x-app-layout>
