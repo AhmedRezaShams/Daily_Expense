db0c3047a03a5a20def4780ab8b18e9768fac981
Arman <mdalarmansorker@gmail.com>
2024-09-07 12:57:49 +0600

    home updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,25 +1,25 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col md:flex-col-2 lg:flex-col-3 gap-3">
-            <div class="card bg-base-100 shadow-xl">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-wrap gap-3 justify-center">
+            <div class="card bg-base-100 shadow-xl max-w-96">
                 <div class="card-body">
                     <h2 class="card-title">{{__('Cash In')}}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                 </div>
             </div>
-            <div class="card bg-base-100 shadow-xl">
+            <div class="card bg-base-100 shadow-xl max-w-96">
                 <div class="card-body">
                     <h2 class="card-title">{{__('Cash Out')}}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                 </div>
             </div>
-            <div class="card bg-base-100 shadow-xl">
+            <div class="card bg-base-100 shadow-xl max-w-96">
+                <div class="card-body">
+                    <h2 class="card-title">{{__('Rest')}}</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                </div>
+            </div>
+            <div class="card bg-base-100 shadow-xl max-w-96">
                 <div class="card-body">
                     <h2 class="card-title">{{__('Rest')}}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
